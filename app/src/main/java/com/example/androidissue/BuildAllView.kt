9b8b8a5716aabed554ce8b6b8816fe177c2c6b77
Brandon Lerner <lernerbrandon@gmail.com>
2020-06-24 14:13:47 -0400

    [Test Project] Reproduce 'build the project' with modules

---
+++ app/src/main/java/com/example/androidissue/BuildAllView.kt
@@ -0,0 +1,8 @@
+package com.example.androidissue
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class BuildAllView constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, breakDefaultConstructor: String)
+    : ConstraintLayout(context, attrs, defStyleAttr)
